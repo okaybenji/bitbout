f487a0d4c57c4fbd4464979500c186580f5f3ca9
Benji Kay <okaybenji@gmail.com>
2015-11-16 14:17:18 -0600

    make player factory and support multiple players on-screen

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,8 @@
 (function game() {
   var nativeWidth = 320;
   var nativeHeight = 180;
-  var player, platforms;
+  var platforms;
+  var players = [];
 
   var resize = function resize() {
     var zoom = $(window).width() / nativeWidth * 100;
@@ -14,19 +15,104 @@
   };
 
   var create = function create() {
-    // work-around for Phaser's shoddy world wrap
+    // work-around for phaser's shoddy world wrap
     var worldMargin = 16;
     game.world.setBounds(-worldMargin, -worldMargin, nativeWidth + worldMargin * 2, nativeHeight + worldMargin * 2);
     
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    (function addPlayer() {
-      player = game.add.sprite(8, 16, 'square');
+    var cursors = game.input.keyboard.createCursorKeys();
+
+    var createPlayer = function(config) {
+      config = config || {};
+      var x = config.x || 4;
+      var y = config.y || 8;
+      player = game.add.sprite(x, y, 'square');
       player.scale.setTo(4, 8);
       game.physics.arcade.enable(player);
       player.body.bounce.y = 0.1;
       player.body.gravity.y = 380;
-    }());
+
+      // phaser apparently automatically calls any function named update attached to a sprite!
+      player.update = function() {
+        (function friction() {
+          if (player.body.touching.down) {
+            if (player.body.velocity.x > 0) {
+              player.body.velocity.x -= 4;
+            } else if (player.body.velocity.x < 0) {
+              player.body.velocity.x += 4;
+            }
+          }
+        }());
+
+        (function playerControls() {
+          (function run() {
+            var maxSpeed = 64;
+            switch (true) {
+              // players have less control in the air
+              case cursors.left.isDown:
+                if (player.body.touching.down) {
+                  player.body.velocity.x = Math.max(player.body.velocity.x - 8, -maxSpeed);
+                } else {
+                  player.body.velocity.x = Math.max(player.body.velocity.x - 3, -maxSpeed);
+                }
+                break;
+              case cursors.right.isDown:
+                if (player.body.touching.down) {
+                  player.body.velocity.x = Math.min(player.body.velocity.x + 8, maxSpeed);
+                } else {
+                  player.body.velocity.x = Math.min(player.body.velocity.x + 3, maxSpeed);
+                }
+                break;
+            }
+          }());
+
+          (function jump() {
+            if (cursors.up.isDown) {
+              switch (true) {
+                case player.body.touching.down:
+                  player.body.velocity.y = -200;
+                  break;
+                // wall jumps
+                case player.body.touching.left:
+                  player.body.velocity.y = -240;
+                  player.body.velocity.x = 90;
+                  break;
+                case player.body.touching.right:
+                  player.body.velocity.y = -240;
+                  player.body.velocity.x = -90;
+                  break;
+              }
+            }
+          }());
+
+          (function duck() {
+            // is this bad practice? adding boolean to phaser's cursors
+            if (typeof cursors.down.wasDown === 'undefined') {
+              cursors.down.wasDown = false;
+            }
+
+            if (cursors.down.isDown) {
+              player.scale.setTo(4, 4);
+              if (!cursors.down.wasDown) {
+                player.y += 8;
+              }
+              cursors.down.wasDown = true;
+            } else if (cursors.down.wasDown) {
+              cursors.down.wasDown = false;
+              player.y -= 8;
+              player.scale.setTo(4, 8);
+            }
+          }());
+        }());
+      };
+
+      return player;
+    };
+
+    players.push(createPlayer());
+    players.push(createPlayer({x: 90, y: 90}));
+    players.push(createPlayer({x: 306, y: 16}));
 
     (function buildLevel() {
       platforms = game.add.group();
@@ -52,81 +138,16 @@
   };
 
   var update = function update() {
-    game.physics.arcade.collide(player, platforms);
-    game.world.wrap(player, 0, true);
-    
-    (function friction() {
-      if (player.body.touching.down) {
-        if (player.body.velocity.x > 0) {
-          player.body.velocity.x -= 4;
-        } else if (player.body.velocity.x < 0) {
-          player.body.velocity.x += 4;
+    players.forEach(function(player, i) {
+      game.physics.arcade.collide(player, platforms);
+      game.world.wrap(player, 0, true);
+      players.forEach(function(otherPlayer, j) {
+        // let players collide with each other, but not with themselves
+        if (i !== j) {
+          game.physics.arcade.collide(player, otherPlayer);
         }
-      }
-    }());
-
-    (function playerControls() {
-      cursors = game.input.keyboard.createCursorKeys();
-
-      (function run() {
-        var maxSpeed = 64;
-        switch (true) {
-          // players have less control in the air
-          case cursors.left.isDown:
-            if (player.body.touching.down) {
-              player.body.velocity.x = Math.max(player.body.velocity.x - 8, -maxSpeed);
-            } else {
-              player.body.velocity.x = Math.max(player.body.velocity.x - 3, -maxSpeed);
-            }
-            break;
-          case cursors.right.isDown:
-            if (player.body.touching.down) {
-              player.body.velocity.x = Math.min(player.body.velocity.x + 8, maxSpeed);
-            } else {
-              player.body.velocity.x = Math.min(player.body.velocity.x + 3, maxSpeed);
-            }
-            break;
-        }
-      }());
-
-      (function jump() {
-        if (cursors.up.isDown) {
-          switch (true) {
-            case player.body.touching.down:
-              player.body.velocity.y = -200;
-              break;
-            // wall jumps
-            case player.body.touching.left:
-              player.body.velocity.y = -240;
-              player.body.velocity.x = 90;
-              break;
-            case player.body.touching.right:
-              player.body.velocity.y = -240;
-              player.body.velocity.x = -90;
-              break;
-          }
-        }
-      }());
-
-      (function duck() {
-        // is this bad practice? adding boolean to phaser's cursors
-        if (typeof cursors.down.wasDown === 'undefined') {
-          cursors.down.wasDown = false;
-        }
-
-        if (cursors.down.isDown) {
-          player.scale.setTo(4, 4);
-          if (!cursors.down.wasDown) {
-            player.y += 8;
-          }
-          cursors.down.wasDown = true;
-        } else if (cursors.down.wasDown) {
-          cursors.down.wasDown = false;
-          player.y -= 8;
-          player.scale.setTo(4, 8);
-        }
-      }());
-    }());
+      });
+    });
   };
 
   var game = new Phaser.Game(nativeWidth, nativeHeight, Phaser.AUTO, '#game', {
