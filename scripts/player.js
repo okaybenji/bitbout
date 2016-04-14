73a5ef8aa856ae4e8e33e4aad7526aa0ee4f3700
okaybenji <okaybenji@gmail.com>
2016-04-13 22:12:06 -0500

    allow scarf x to scale -1 to 1

--- scripts/player.js
+++ scripts/player.js
@@ -235,7 +235,7 @@ var createPlayer = function createPlayer(game, options, onDeath) {
   player.scarf = game.add.sprite(-4, -1, settings.color + 'Scarf');
   player.scarf.animations.add('scarf');
   player.scarf.animations.play('scarf', 32/3, true);
-  player.scarf.setScaleMinMax(1, 1, 1, 1);
+  player.scarf.setScaleMinMax(-1, 1, 1, 1);
   player.addChild(player.scarf);
 
   // track health
