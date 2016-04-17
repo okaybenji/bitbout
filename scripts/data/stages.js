6d726d54bf3c1a04d51fc927341407835c2f229c
okaybenji <okaybenji@gmail.com>
2016-04-17 16:29:46 -0500

    animate hangar as elevator shaft

--- scripts/data/stages.js
+++ scripts/data/stages.js
@@ -1,4 +1,44 @@
 var stages = [{
+  "name": "Forest",
+  "theme": "forest",
+  "backgroundColor": "#000",
+  "platforms": {
+    "positions": [
+      [27, 6],
+      [10, 13],
+      [44, 13],
+      [5, 22],
+      [49, 22],
+      [18, 31],
+      [27, 31],
+      [36, 31],
+      [5, 44],
+      [49, 44],
+      [27, 60]
+    ],
+    "color": "white"
+  },
+  "backgrounds": [{
+    "image": "forest"
+  }, {
+    "image": "forestBg1",
+    "pulse": true,
+    "pulseDuration": 4000,
+  }, {
+    "image": "forestBg2",
+    "pulse": true,
+    "pulseDelay": 3000,
+    "pulseDuration": 5000,
+  }],
+  "foregrounds": [],
+  "spawnPoints": [
+    { "x": 14, "y": 6 },
+    { "x": 48, "y": 6 },
+    { "x": 9, "y": 15 },
+    { "x": 54, "y": 15 }
+  ],
+  "uiColor": "#28D6F1"
+},{
   "name": "Waterfall",
   "theme": "waterfall",
   "backgroundColor": "#000",
@@ -40,80 +80,6 @@ var stages = [{
   ],
   "uiColor": "#28F129"
 },{
-  "name": "Forest",
-  "theme": "forest",
-  "backgroundColor": "#000",
-  "platforms": {
-    "positions": [
-      [27, 6],
-      [10, 13],
-      [44, 13],
-      [5, 22],
-      [49, 22],
-      [18, 31],
-      [27, 31],
-      [36, 31],
-      [5, 44],
-      [49, 44],
-      [27, 60]
-    ],
-    "color": "white"
-  },
-  "backgrounds": [{
-    "image": "forest"
-  }, {
-    "image": "forestBg1",
-    "pulse": true,
-    "pulseDuration": 4000,
-  }, {
-    "image": "forestBg2",
-    "pulse": true,
-    "pulseDelay": 3000,
-    "pulseDuration": 5000,
-  }],
-  "foregrounds": [],
-  "spawnPoints": [
-    { "x": 14, "y": 6 },
-    { "x": 48, "y": 6 },
-    { "x": 9, "y": 15 },
-    { "x": 54, "y": 15 }
-  ],
-  "uiColor": "#28D6F1"
-},{
-  "name": "Hangar",
-  "theme": "hangar",
-  "backgroundColor": "#000",
-  "gravity": 150,
-  "platforms": {
-    "positions": [
-      [8, 34],
-      [12, 34],
-      [22, 34],
-      [31, 34],
-      [41, 34],
-      [46, 34],
-    ],
-    "color": "white"
-  },
-  "backgrounds": [{
-    "image": "hangar"
-  }, {
-    "image": "hangarBg",
-    "animated": true,
-    "frames": [0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 2, 1],
-    "animSpeed": 10,
-  }],
-  "foregrounds": [{
-    "image": "clear",
-  }],
-  "spawnPoints": [
-    { "x": 10, "y": 27 },
-    { "x": 19, "y": 27 },
-    { "x": 41, "y": 27 },
-    { "x": 50, "y": 27 }
-  ],
-  "uiColor": "#8D8D8D"
-},{
   "name": "Tomb",
   "theme": "tomb",
   "backgroundColor": "#000",
@@ -140,6 +106,43 @@ var stages = [{
     { "x": 48, "y": 24 }
   ],
   "uiColor": "#783E08"
+},{
+  "name": "Hangar",
+  "theme": "hangar",
+  "backgroundColor": "#000",
+  "gravity": 150,
+  "platforms": {
+    "positions": [
+      [8, 34],
+      [12, 34],
+      [22, 34],
+      [31, 34],
+      [41, 34],
+      [46, 34],
+    ],
+    "color": "white"
+  },
+  "backgrounds": [{
+    "image": "hangar"
+  }, {
+    "image": "hangarBg1",
+    "animated": true,
+    "frames": [0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 2, 1],
+    "animSpeed": 10,
+  }, {
+    "image": "hangarBg2",
+    "animated": true,
+  }],
+  "foregrounds": [{
+    "image": "clear",
+  }],
+  "spawnPoints": [
+    { "x": 10, "y": 27 },
+    { "x": 19, "y": 27 },
+    { "x": 41, "y": 27 },
+    { "x": 50, "y": 27 }
+  ],
+  "uiColor": "#8D8D8D"
 }];
 
 module.exports = stages;
